184caef7d2e366fbb7137a360b237ce54f7c924e
ti-ff <ti-ff@DESKTOP-8EQCTPQ>
2017-10-02 20:31:42 -0500

    Add project files.

---
+++ App5/App5/Page1.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App5
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page2());
+        }
+        private void Button_Clicked5(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page3());
+        }
+    }
+}
---
+++ App5/App5/Page2.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App5
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+        private void Guardar_Clicked_1(object sender, EventArgs e)
+        {
+            DisplayAlert("hola", nombre1.Text + "" + apellido2.Text + "" + direccion3.Text + "" + telefono4.Text + "" + carrera5.Text + "" + semestre6.Text + "" + matricula7.Text + "" + correo8.Text + "" + github9.Text, "Good");
+        }
+
+        private void Button_Clicked1(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page3());
+        }
+        private void Button_Clicked2(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page1());
+        }
+
+    }
+}
+   
